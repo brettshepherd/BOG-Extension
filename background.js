ae9ae3701518c43b246538ce59e4d15f8a6c6a0b
Brett Shepherd <brettshepherd99@gmail.com>
2021-04-18 23:05:26 -0400

    updated manifest to v3

--- background.js
+++ background.js
@@ -1,13 +1,10 @@
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener((tab) => {
-  // Send a message to the active tab
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {
-      message: "clicked_browser_action"
-    });
-  });
-});
+// chrome.runtime.onInstalled.addListener( ()=> {
+//   chrome.contextMenus.create({
+//     "id": "sampleContextMenu",
+//     "title": "Sample Context Menu",
+//     "contexts": ["selection"]
+//   });
+// });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === "open_chart") {
